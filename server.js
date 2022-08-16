5520c542d01063c0390b989f234bb613a52f4986
real-na <naogko@163.com>
2022-08-16 19:52:01 +0800

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+let http = require('http');
+
+let users = [
+  { id: 1, name: 'zee'},
+  {id: 2, name: 'nunew'},
+  { id: 3, name: 'zeenunew'}
+]
+let server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.url === '/api/users') {
+    res.end(JSON.stringify(users));
+  } else {
+    res.end('Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('3000端口监听成功')
+});
