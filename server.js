167333f38a9a191a72ea32e527cd00f74d892789
real-na <naogko@163.com>
2022-08-19 15:24:54 +0800

    testwebhook

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 let http = require('http');
 
 let users = [
-  { id: 1, name: 'zee'},
-  {id: 2, name: 'nunew'},
-  { id: 3, name: 'zeenunew'}
+  { id: 11, name: 'zee'},
+  {id: 22, name: 'nunew'},
+  { id: 33, name: 'zeenunew'}
 ]
 let server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
